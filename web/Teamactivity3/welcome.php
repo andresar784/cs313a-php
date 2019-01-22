55f1d462a47358fabf73717e1f9f94526bdb0164
Andres Rodriguez <rod16015@byui.edu>
2019-01-22 20:43:19 -0300

    added minnors changes

--- web/Teamactivity3/welcome.php
+++ web/Teamactivity3/welcome.php
@@ -1,14 +1,23 @@
+<?php if(isset(echo $_POST["name"])){
+    ?>
+
+    
 <!DOCTYPE html>
 <html>
 <head>
     <title>Welcome php</title>
 </head>
 <body>
-    Name <?php echo $_POST["name"]; ?><br>
+     Name <?php echo $_POST["name"]; ?><br>
     Mail address <?php echo $_POST["email"];?><br>
     Major <?php echo $_POST["major"];?><br>
     Comments <?php echo $_POST["comment"];?>
-
-    
+    Visited Continents <?php echo $_POST["continents"]; ?><br>
+    <?php foreach($_POST["continents"] as $countries){
+        echo $countries;
+        echo ", ";
+    }
 </body>
 </html>>
+
+<?php }
