d09ac6b9db91b6b074b23b8ce9b451b3877b1e79
Andres Rodriguez <rod16015@byui.edu>
2019-01-22 19:36:22 -0300

    added minnors changes

--- web/Teamactivity3/welcome.php
+++ web/Teamactivity3/welcome.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Welcome php</title>
 </head>
 <body>
     Name <?php echo $_POST["name"]; ?><br>
     Mail address<?php echo $_POST["email"];?><br>
     Major<?php echo $_POST["major"];?><br>
-    Commments<?php echo $_POST["comments"]
+    Commments<?php echo $_POST["comments"];?>
 
     
 </body>
