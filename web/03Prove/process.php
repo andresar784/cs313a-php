fbc2df98f5ee6c69216c804fbfff0dd51b90b2ca
Andres Rodriguez <rod16015@byui.edu>
2019-01-28 20:36:02 -0300

    Added some little things

--- web/03Prove/process.php
+++ web/03Prove/process.php
@@ -87,14 +87,14 @@ session_start();
 			  
               if($arrlength > 0)
               {
-					echo "<ul>";
-					for($x = 0; $x < $arrlength; $x++) {
-					echo "<li>" . $_SESSION["add_cart"][$x] . " juice</li>";
-					echo "<br/>";
-					echo "<button class='remove btn btn-secondary' value=$x>Remove Item From List</button>";
-					echo "<br/>";
+                 <ul>;
+                for($x = 0; $x < $arrlength; $x++) {
+					      echo "<li>" . $_SESSION["add_cart"][$x] . " juice</li>";
+					      echo "<br/>";
+					      echo "<button class='remove btn btn-secondary' value=$x>Remove Item From List</button>";
+					      echo "<br/>";
                 }
-                echo "</ul>";
+                </ul>;
               };
               ?>
         </div>
