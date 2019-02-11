c9f7efd817e35cb1afdcf2642b17a50ba4ea5381
Andres Rodriguez <rod16015@byui.edu>
2019-02-11 15:21:24 -0300

    Corrected minor issues

--- web/bus/test.php
+++ web/bus/test.php
@@ -12,6 +12,13 @@
 			$dbName = ltrim($dbOpts["path"],'/');
 
 			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+			$statement = $db->prepare("SELECT id, place_name FROM place";
+			$statement->execute();
+			while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+				<p>
+					$row['id'] . $row['place_name'];
+				</p>
+		}
 
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}
@@ -20,13 +27,6 @@
 		echo 'Error!: ' . $ex->getMessage();
 		die();
 		}
-	$statement = $db->prepare("SELECT id, place_name FROM place";
-	$statement->execute();
-	while($row = $statement->fetch(PDO::FETCH_ASSOC))
-	{	
-		<p>
-		$row['id'] . $row['place_name'];
-		</p>
-	}
+	
 	
 ?>
