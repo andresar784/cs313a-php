46a87c60a26480374aba0702a5e2e5d18c1b7e9f
Andres Rodriguez <rod16015@byui.edu>
2019-02-11 15:12:44 -0300

    Corrected minor issues

--- web/bus/test.php
+++ web/bus/test.php
@@ -2,9 +2,9 @@
 	require_once('bd.php');
 	$db = DB::init();
 	$statement = $db->prepare("SELECT id, place_name FROM place";
-	$statement->execute();
+	$statement->execute($statement);
 	while($row = $statement->fetch(PDO::FETCH_ASSOC))
 	{
-	echo $row['id'] . $row['place_name']
+	echo $row['id'] . $row['place_name'];
 	}
 ?>
