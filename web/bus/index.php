e288fc0c77caa02b06017914fb2321ade4ae733d
Andres Rodriguez <rod16015@byui.edu>
2019-02-11 11:16:31 -0300

    Corrected minor issues

--- web/bus/index.php
+++ web/bus/index.php
@@ -35,7 +35,7 @@
 		die();
 		}
 
-		$statement = $db->prepare("SELECT id, name FROM place");
+		/* $statement = $db->prepare("SELECT id, name FROM place");
 		$statement->execute();
 		$place_1 = SELECT * FROM place WHERE placeid = 1;
 		$place_2 = SELECT * FROM place WHERE placeid = 2;
@@ -43,7 +43,7 @@
 		$place_4 = SELECT * FROM place WHERE placeid = 4;
 		$place_5 = SELECT * FROM place WHERE placeid = 5;
 
-		echo $place_1;
+		echo $place_1; */
 ?>
 
 
