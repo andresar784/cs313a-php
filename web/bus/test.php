1e2c2e456e457ba9d29bcdb5e87644e72e80226d
Andres Rodriguez <rod16015@byui.edu>
2019-02-11 16:51:55 -0300

    Corrected minor issues

--- web/bus/test.php
+++ web/bus/test.php
@@ -1,35 +1,14 @@
 <?php
-	try
-		{	
-			$dbUrl = getenv('DATABASE_URL');
-
-			$dbOpts = parse_url($dbUrl);
-
-			$dbHost = $dbOpts["host"];
-			$dbPort = $dbOpts["port"];
-			$dbUser = $dbOpts["user"];
-			$dbPassword = $dbOpts["pass"];
-			$dbName = ltrim($dbOpts["path"],'/');
-
-			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-			
-			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		}
-		catch (PDOException $ex)
-		{
-		echo 'Error!: ' . $ex->getMessage();
-		die();
-		}
-		
-
-		$statement = $db->prepare("SELECT id, place_name FROM place");
-		$statement->execute();
-		$place_name = array();
-		while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
-			$place_name[$row['id']] = $row['place_name'];
-		}
-		var_dump($place_name);
-		echo $place_name[1];
+	require_once('bd.php');
+	$db = DB::init();
+	$statement = $db->prepare("SELECT id, place_name FROM place");
+	$statement->execute();
+	$place_name = array();
+	while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+		$place_name[$row['id']] = $row['place_name'];
+	}
+	var_dump($place_name);
+	echo $place_name[1];
 		
 				
 				
