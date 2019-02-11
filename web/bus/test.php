e1d6938c1e2db60d7bfa2a0f147f3791da12882e
Andres Rodriguez <rod16015@byui.edu>
2019-02-11 15:26:15 -0300

    Corrected minor issues

--- web/bus/test.php
+++ web/bus/test.php
@@ -1,13 +1,5 @@
 <?php
-	$txt1 = "Learn PHP";
-	$txt2 = "W3Schools.com";
-	$x = 5;
-	$y = 4;
-
-	echo "<h2>" . $txt1 . "</h2>";
-	echo "Study PHP at " . $txt2 . "<br>";
-	echo $x + $y;
-	/* try
+	try
 		{	
 			$dbUrl = getenv('DATABASE_URL');
 
@@ -21,10 +13,11 @@
 
 			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 			$statement = $db->prepare("SELECT id, place_name FROM place";
+	
 			$statement->execute();
 			while($row = $statement->fetch(PDO::FETCH_ASSOC)){
 				<p>
-					$row['id'] . $row['place_name'];
+					echo $row['id'] . $row['place_name'];
 				</p>
 		}
 
@@ -34,7 +27,7 @@
 		{
 		echo 'Error!: ' . $ex->getMessage();
 		die();
-		} */
+		}
 	
 	
 ?>
