f9f0ddc1d1d6ae7475703a4bac117c08e42e75bc
Andres Rodriguez <rod16015@byui.edu>
2019-02-11 15:08:04 -0300

    Corrected minor issues

--- web/bus/test.php
+++ web/bus/test.php
@@ -1,5 +1,7 @@
 <?php
-	echo = "wrong";
+	require_once('bd.php');
+	$db = DB::init();
+	echo = 'wrong';
 	$statement = $db->prepare("SELECT id, place_name FROM places";
 	$statement->execute();
 	while($row = $statement->fetch(PDO::FETCH_ASSOC))
